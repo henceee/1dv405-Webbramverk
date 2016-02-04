bd589c9d7e4d7368a842676c4028bc16c0ee3d4c
henceee <hg222dv@student.lnu.se>
2016-02-04 11:07:24 +0100

    :wave:

---
+++ Registration-App/reg1dv450/workspace/reg1dv450/app/controllers/session_controller.rb
@@ -0,0 +1,23 @@
+class SessionController < ApplicationController
+    
+    def login
+        u = User.find_by_email(params[:email])
+        if u && u.authenticate(params[:password])
+              session[:userid] = u.id
+              #warning: ADMIN must have id 0, rake db:seed to get admin and sample user
+              if(u.id === 0)
+                  redirect_to apikeyadmin_path
+              else
+                  redirect_to apikey_path
+              end
+        else      
+            flash[:notice] = "Incorrect login details."
+            redirect_to root_path
+        end
+    end
+    
+    def logout
+        session[:userid] = nil
+        redirect_to root_path, notice: "You've been logged out"
+    end
+end
